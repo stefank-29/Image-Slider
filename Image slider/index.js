efaed415e138f21e6885f7b135baefcfedc2082c
stefan <stefankaraferovic@gmail.com>
2020-08-18 14:06:47 +0200

    Added 5sec timeout

--- Image slider/index.js
+++ Image slider/index.js
@@ -4,6 +4,7 @@ const imageSlider = (() => {
   const sliderImage = document.querySelector('#slider img');
   const dots = document.querySelector('#dots');
   let imageIndex = 1;
+  let interval;
 
   function _deactivateAllDots() {
     dots.querySelectorAll('i').forEach((dot) => {
@@ -16,27 +17,33 @@ const imageSlider = (() => {
     currDot.classList.remove('fa-circle');
     currDot.classList.add('fa-dot-circle');
   }
+  function showNextImg5sec() {
+    clearInterval(interval);
+    interval = setInterval(nextImage, 5000);
+  }
 
   function previousImage() {
     if (imageIndex !== 1) {
-      imageIndex -= 1;
+      imageIndex--;
     } else {
       imageIndex = 9;
     }
     _deactivateAllDots();
     sliderImage.setAttribute('src', `./images/cover${imageIndex}.jpg`);
     _activateDot();
+    showNextImg5sec();
   }
 
   function nextImage() {
     if (imageIndex !== 9) {
-      imageIndex += 1;
+      imageIndex++;
     } else {
       imageIndex = 1;
     }
     _deactivateAllDots();
     sliderImage.setAttribute('src', `./images/cover${imageIndex}.jpg`);
     _activateDot();
+    showNextImg5sec();
   }
 
   function showImageOnDot(dot) {
@@ -45,6 +52,7 @@ const imageSlider = (() => {
     _deactivateAllDots();
     sliderImage.setAttribute('src', `./images/cover${imageIndex}.jpg`);
     _activateDot();
+    showNextImg5sec();
   }
 
   document.querySelector('#left').addEventListener('click', previousImage);
@@ -68,5 +76,8 @@ const imageSlider = (() => {
     nextImage,
     setImageIndex,
     getImageIndex,
+    showNextImg5sec,
   };
 })();
+
+imageSlider.onload = imageSlider.showNextImg5sec();
