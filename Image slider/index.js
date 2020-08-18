59866681147ac075641103a020c9e5f068908b4b
stefan <stefankaraferovic@gmail.com>
2020-08-18 13:22:55 +0200

    Dots active image

--- Image slider/index.js
+++ Image slider/index.js
@@ -1,14 +1,27 @@
 // todo animacija za promenu slika
 const imageSlider = (() => {
   const sliderImage = document.querySelector('#slider img');
+  const dots = document.querySelector('#dots');
   let imageIndex = 1;
+
+  function _deactivateAllDots() {
+    dots.querySelectorAll('i').forEach((dot) => {
+      dot.classList.remove('fa-dot-circle');
+      dot.classList.add('fa-circle');
+    });
+  }
+
   function leftImage() {
     if (imageIndex !== 1) {
       imageIndex -= 1;
     } else {
       imageIndex = 9;
     }
+    _deactivateAllDots();
     sliderImage.setAttribute('src', `./images/cover${imageIndex}.jpg`);
+    const currDot = dots.querySelector(`[data-index="${imageIndex}"]`);
+    currDot.classList.remove('fa-circle');
+    currDot.classList.add('fa-dot-circle');
   }
 
   function rightImage() {
@@ -17,7 +30,11 @@ const imageSlider = (() => {
     } else {
       imageIndex = 1;
     }
+    _deactivateAllDots();
     sliderImage.setAttribute('src', `./images/cover${imageIndex}.jpg`);
+    const currDot = dots.querySelector(`[data-index="${imageIndex}"]`);
+    currDot.classList.remove('fa-circle');
+    currDot.classList.add('fa-dot-circle');
   }
 
   document.querySelector('#left').addEventListener('click', leftImage);
