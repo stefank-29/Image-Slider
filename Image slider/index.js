53fab5a867e74bc319bffd47f1f25548d542322a
stefan <stefankaraferovic@gmail.com>
2020-08-18 12:18:06 +0200

    Added pictures and image style

---
+++ Image slider/index.js
@@ -0,0 +1,11 @@
+// todo animacija za promenu slika
+const imageSlider = (() => {
+  function leftImage() {}
+
+  function rightImage() {}
+
+  return {
+    leftImage,
+    rightImage,
+  };
+})();
