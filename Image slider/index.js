b9b404d50149cb0652cc3549881018342d293c04
stefan <stefankaraferovic@gmail.com>
2020-08-18 12:43:00 +0200

    Left and right arrow functions

--- Image slider/index.js
+++ Image slider/index.js
@@ -1,11 +1,32 @@
 // todo animacija za promenu slika
 const imageSlider = (() => {
-  function leftImage() {}
+  const sliderImage = document.querySelector('#slider img');
+  let imageIndex = 1;
+  function leftImage() {
+    imageIndex -= 1;
+    sliderImage.setAttribute('src', `./images/cover${imageIndex}.jpg`);
+  }
 
-  function rightImage() {}
+  function rightImage() {
+    imageIndex += 1;
+    sliderImage.setAttribute('src', `./images/cover${imageIndex}.jpg`);
+  }
+
+  document.querySelector('#left').addEventListener('click', leftImage);
+  document.querySelector('#right').addEventListener('click', rightImage);
+
+  function setImageIndex(index) {
+    imageIndex = index;
+  }
+
+  function getImageIndex() {
+    return imageIndex;
+  }
 
   return {
     leftImage,
     rightImage,
+    setImageIndex,
+    getImageIndex,
   };
 })();
